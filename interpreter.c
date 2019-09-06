1589bcca5244f11e3d4e09dcde33be06154f48e6
Reisande <mryabk2@uic.edu>
2019-09-06 13:22:42 -0500

    Elementary type checking and node processing has been implemented. The next step is to implement state properly, and allow for use fo variables. Even though technically lisp is supposed to support shadowing variables, I am not.

--- interpreter.c
+++ interpreter.c
@@ -1,7 +1,9 @@
 #include "ast.h"
 
+// Change values to custom type
 typedef struct value {
-  enum valueEnum { intVal, string, closureVal, listVal, boolVal } valueType;
+	// TODO: does defVal make logical sense?
+  enum valueEnum { intVal, stringVal, lambdaVal, listVal, boolVal, defVal } valueType;
   void *pVal; // the actual value of the val(int, bool, etc.)
 } value;
 
@@ -12,9 +14,17 @@ typedef struct listNode {
   char * name;
 } listNode;
 
+typedef struct lambdaNode {
+	// the body of the arguments
+	node *body; 
+	// the single argument. Multiple arguments could be implemented as multiple nested lambdas
+	char *argument;
+} lambdaNode;											
+
 typedef struct closure closure;
 typedef struct stateNode stateNode;
 
+// TODO: why did I have closures/state again?
 struct closure {
   node *lambda;
   stateNode *stateVal;
@@ -22,19 +32,26 @@ struct closure {
 
 struct stateNode {
   char *name;
-  value Val ;
+  value Val;
   stateNode *next;
 };
 
 value *process(node *root);
 value *processIf(node *root);
 
-value *processLambda(node *root) {
-  return NULL;
-}
-
 value *processDef(node *root) {
-  return NULL;
+	// TODO: check that the new value is not reassigning already assigned value.
+	// If it is, it will be a type error, because shadowing existing values seems
+	// unnecessarily complicated
+	if(process(root->children[0]) == stringVal) {
+		value *returnDefVal;
+		returnDefVal->valueType = defVal;
+		returnDefVal->pVal = process(root->children[1]);
+		return returnDefVal;
+	}
+	else {
+		return NULL;
+	}
 }
 
 value *processIf(node *root) {
@@ -47,27 +64,36 @@ value *processIf(node *root) {
   }
 }
 
-value *processList(node *root) {
+listNode *processList(node *root) {
   if(root != NULL) {
     listNode *newNode = malloc(sizeof(node));
     newNode->Val = process(root->children[0]);
     newNode->next = processList(root->children[1]);
+		return newNode;
   }
   else {
     return NULL;
   }
 }
 
-value *process(node *root) {
-  value *returnVal;
-  int intermediaryInt = -99999;
-  switch (root->token) {
+value *_processList(node *root) {
+	value *headNode = (value *) malloc(sizeof(value));
+  headNode->valueType = listVal;
+  headNode->pVal = (void *)processList(root);
+	return headNode;
+}
+
+value *process(node *root, stateNode *state) {
+  value *returnVal = (value *) malloc(sizeof(value));
+  int intermediaryInt = 0; // default value TODO: make a wrapper option struct
+	if(root != NULL) {
+		switch (root->token) {
     case NUM:
       returnVal->valueType = intVal;
       returnVal->pVal = &(root->value);
       break;
     case STR:
-      returnVal->valueType = string;
+      returnVal->valueType = stringVal;
       returnVal->pVal = root->name;
       break;
     case BOOL:
@@ -76,35 +102,58 @@ value *process(node *root) {
       break;
     case ADD:
       returnVal->valueType = intVal;
-      intermediaryInt = (*((int *)(process(root->children[0])->pVal)) + (*((int *)(process(root->children[0])->pVal))));
+      intermediaryInt = (*((int *)(process(root->children[0])->pVal)) +
+												 (*((int *)(process(root->children[0])->pVal))));
       returnVal->pVal = &intermediaryInt;
       break;
     case SUB:
       returnVal->valueType = intVal;
-      intermediaryInt = (*((int *)(process(root->children[0])->pVal)) - (*((int *)(process(root->children[0])->pVal))));
+      intermediaryInt = (*((int *)(process(root->children[0])->pVal)) -
+												 (*((int *)(process(root->children[0])->pVal))));
       returnVal->pVal = &intermediaryInt;
       break;
     case MUL:
       returnVal->valueType = intVal;
-      intermediaryInt = (*((int *)(process(root->children[0])->pVal)) * (*((int *)(process(root->children[0])->pVal))));
+      intermediaryInt = (*((int *)(process(root->children[0])->pVal)) *
+												 (*((int *)(process(root->children[0])->pVal))));
       returnVal->pVal = &intermediaryInt;
       break;
     case DIV:
       returnVal->valueType = intVal;
-      intermediaryInt = (*((int *)(process(root->children[0])->pVal)) / (*((int *)(process(root->children[0])->pVal))));
+      intermediaryInt = (*((int *)(process(root->children[0])->pVal)) /
+												 (*((int *)(process(root->children[0])->pVal))));
       returnVal->pVal = &intermediaryInt;
       break;
     case DEF:
-      return processDef(root);
+      returnVal = processDef(root);
+			break;
     case IF:
-      return processIf(root);
+      returnVal = processIf(root);
+		  break;
     case LIST:
-      return processList(root);
+			returnVal->valueType = listVal;
+      returnVal->pVal = _processList(root);
+			break;
     case LAMBDA:
-      return processLambda(root);
+			// how would I go about actually applying a lambda to a value?
+			returnVal->valueType = lambdaVal;
+			// TODO: this feels off to me, although I guess it does make sense for
+			// lambdas to be terminals
+			returnVal->pVal = root;
+			break;
     default:
       // should do nothing, this a stuck value
-      return NULL; // should be an error value, but is not a very good one;
-  }
-  return returnVal;
+      returnVal =  NULL; // should be an error value, but is not a very good one;
+		}
+		return returnVal;
+	}
+	else {
+		printf("Some compilation error occurred\n");
+		return NULL;
+	}
+}
+
+int main() {
+
+	return 0;
 }
